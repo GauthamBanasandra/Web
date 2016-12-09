bc24491226cb42c288ddc088ec11fd35f71cd46e
GauthamBanasandra <gautham.bangalore@gmail.com>
2016-12-09 21:04:49 +0530

    RSS news. Using marquee tag to scroll the news.

---
+++ lab/6/get_news.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gautham
+ * Date: 9/12/16
+ * Time: 8:03 PM
+ */
+echo file_get_contents('rss.xml');
---
+++ lab/6/js/hot_news.js
@@ -0,0 +1,40 @@
+/**
+ * Created by gautham on 9/12/16.
+ */
+function getNews()
+{
+    $.get('get_news.php', function (res)
+    {
+        var parser = new DOMParser();
+        var doc = parser.parseFromString(res, 'application/xml').documentElement;
+
+        var newsLinks = getNewsLinks(doc);
+
+        insertNews(newsLinks);
+        // debug.
+        console.log(newsLinks);
+    });
+}
+
+function getNewsLinks(xmlDoc)
+{
+    var newsLinks = {};
+    var items = xmlDoc.getElementsByTagName('item');
+    for (var i = 0; i < items.length; ++i)
+    {
+        var title = items[i].getElementsByTagName('title')[0];
+        var link = items[i].getElementsByTagName('link')[0];
+        newsLinks[link.innerHTML] = title.innerHTML;
+    }
+
+    return newsLinks;
+}
+
+function insertNews(newsLinks)
+{
+    var keys = Object.keys(newsLinks);
+    for (var i = 0; i < keys.length; ++i)
+        $('#news_scroll').append('<a class="btn btn-info" href="' + keys[i] + '">' + newsLinks[keys[i]] + '</a>&nbsp;&nbsp;');
+}
+
+getNews();
